eac0f9061dc0c2efffbc1fa4c539d16446b4d8fd
kytrack <70961365+kytrack@users.noreply.github.com>
2023-05-18 14:20:31 +0200

    Add files via upload

---
+++ Hegycsucsosztaly.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hegyescsucsos
+{
+    class Hegycsucsosztaly
+    {
+        string nev;
+        string hegyseg;
+        int magassag;
+
+        public Hegycsucsosztaly(string nev, string hegyseg, int magassag)
+        {
+            this.nev = nev;
+            this.hegyseg = hegyseg;
+            this.magassag = magassag;
+        }
+
+        public string Nev { get => nev;}
+        public string Hegyseg { get => hegyseg;}
+        public int Magassag { get => magassag;}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Hegyescsucsos;
+List<Hegycsucsosztaly> csucsok = new List<Hegycsucsosztaly>();
+
+StreamReader sr = new StreamReader("hegyekMo.txt");
+sr.ReadLine();
+
+while (!sr.EndOfStream)
+{
+    string[] hegyek = sr.ReadLine().Split(";");
+    csucsok.Add(new Hegycsucsosztaly(hegyek[0], hegyek[1],int.Parse(hegyek[2])));
+}
+sr.Close();
+
+Console.WriteLine($"3.feladat: Hegycsúcsok száma: {csucsok.Count()} db");
+
+Console.WriteLine($"4.feladat: Hegycsúcsok átlagos magassága {csucsok.Average(x => x.Magassag)}");
+
+var legmagasabbAdatok = csucsok.OrderByDescending(csucs => csucs.Magassag).First();
+Console.WriteLine($"5.feladat: A legmagasabb hegycsúcs adatai:\n\tNév: {legmagasabbAdatok.Nev}\n\tHegység: {legmagasabbAdatok.Hegyseg}\n\tMagasság: {legmagasabbAdatok.Magassag}m");
+
+Console.WriteLine("6.feladat: Kérek egy magasságot: ");
+int bekertMgassag=int.Parse(Console.ReadLine());
